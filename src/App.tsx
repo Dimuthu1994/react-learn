0e4038034e1efff31a4d85f99f64d75b8a87567c
Dimuthu1994 <dimuthudiss6@gmail.com>
2024-01-30 11:15:11 +0530

    passing childeren

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["galle", "kandy", "matara"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello world</Alert>
     </div>
   );
 }
