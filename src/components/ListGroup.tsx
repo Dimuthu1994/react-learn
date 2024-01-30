223e55d3ae64c75ba6f95c911cde1f34b6379c1b
Dimuthu1994 <dimuthudiss6@gmail.com>
2024-01-30 10:19:47 +0530

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-
 const ListGroup = () => {
   const items = ["galle", "colombo", "kandy"];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li key={index} className="list-group-item">
