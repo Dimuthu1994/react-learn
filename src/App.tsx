b7918dd39df3775fa1de95789828339e9b31d415
Dimuthu1994 <dimuthudiss6@gmail.com>
2024-01-31 14:11:13 +0530

    create data

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,26 @@ function App() {
       });
   };
 
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 0, name: "dimuthu" };
+    setUsers([newUser, ...users]);
+    axios
+      .post("https://jsonplaceholder.typicode.com/users/", newUser)
+      .then((res) => setUsers([res.data, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-danger"> {error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
       <ul className="list-group ">
         {users.map((user) => (
           <li
