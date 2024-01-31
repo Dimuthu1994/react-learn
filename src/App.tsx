4542d20d9c393773b2050e351d4cdeaed163b065
Dimuthu1994 <dimuthudiss6@gmail.com>
2024-01-31 12:46:37 +0530

    showing a loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError, CanceledError } from "axios";
+import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -9,10 +9,11 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
@@ -21,13 +22,18 @@ function App() {
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
       });
+
     return () => controller.abort();
   }, []);
 
   return (
     <>
       {error && <p className="text-danger"> {error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
